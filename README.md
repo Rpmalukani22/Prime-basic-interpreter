daa3784ed8c18f22d3cf369fcf1bb65483b87dd5
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2019-08-19 19:35:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Prime_compiler
+ Created own implementation basic programming language named 'prime' using python.
---
+++ my_lexer.py
@@ -0,0 +1,9 @@
+"""
+Author : Ruchitesh Malukani
+Aim : Create interpreter for new language named 'prime' ( OPEN ENDED PROBLEM )
+"""
+from sly import Lexer
+
+
+class primeLexer(Lexer):
+    tokens = {NAME, NUMBER, STRING, IF, THEN, ELSE ,FOR, FUN, TO, ARROW, EQEQ};
