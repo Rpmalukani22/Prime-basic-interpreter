ffeb805876b7229acf7b570e8856fe282fc5712c
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2019-08-19 20:35:58 +0530

    token definition created

--- my_lexer.py
+++ my_lexer.py
@@ -7,3 +7,19 @@ from sly import Lexer
 
 class primeLexer(Lexer):
     tokens = {NAME, NUMBER, STRING, IF, THEN, ELSE ,FOR, FUN, TO, ARROW, EQEQ}
+    ignore='\t'
+
+    # Token definition
+    IF = r'PRIME_IF'
+    THEN = r'THEN'
+    ELSE = r'ELSE'
+    FOR = r'PRIME_FOR'
+    FUN = r'PRIME_FUN'
+    TO = r'TO'
+    ARROW = r'->'
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    STRING = r'\".*?\"'
+
+    EQEQ = r'=='
+
+
