feb4f0db16c0dddb91ef14bf45de7b6afe1b04e7
Ruchitesh Malukani <ruchiteshmalukani@gmail.com>
2019-08-19 20:41:30 +0530

    Lexical analysis completed

--- my_lexer.py
+++ my_lexer.py
@@ -5,7 +5,7 @@ Aim : Create interpreter for new language named 'prime' ( OPEN ENDED PROBLEM )
 from sly import Lexer
 
 
-class primeLexer(Lexer):
+class PrimeLexer(Lexer):
     tokens = {NAME, NUMBER, STRING, IF, THEN, ELSE ,FOR, FUN, TO, ARROW, EQEQ}
     ignore='\t'
 
@@ -19,7 +19,34 @@ class primeLexer(Lexer):
     ARROW = r'->'
     NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
     STRING = r'\".*?\"'
-
     EQEQ = r'=='
 
+    #literals
+    literals = {'=', '+', '-', '/', '*', '(', ')', ',', ';'}
 
+    @_(r'\d+')
+    def NUMBER(self, t):
+        t.value = int(t.value)
+        return t
+
+    @_(r'#.*')
+    def COMMENT(self, t):
+        pass
+
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno = t.value.count('\n')
+
+
+if __name__ == '__main__':
+    lexer = PrimeLexer()
+    env = {}
+    while True:
+        try:
+            text = input('Prime > ')
+        except EOFError:
+            break
+        if text:
+            lex = lexer.tokenize(text)
+            for token in lex:
+                print(token)
